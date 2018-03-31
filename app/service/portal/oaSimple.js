99c4c5802a4297c55f6a0dd5b22d968b19310425
dzm <dzm1846907072@gmail.com>
2018-03-31 11:25:24 +0800

    简单例子

---
+++ app/service/portal/oaSimple.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const Service = require('egg').Service;
+const rds = require('ali-rds');
+
+class OaService extends Service {
+    /* 
+    1、获取全部oauser
+    2、比对oa和运营的用户
+       （1）根据oa： lastname  => 运营： user_name
+       （2）
+          - 如果一个都没找到  oa： loginid => 运营： user_account
+              - 如果一个都没有就结束
+              - 如果多个匹配上打log
+          - 如果有name有多条对应上， 则再次匹配loginid
+              - 如果一个都没有就结束
+              - 匹配多个打log
+      （3）导出email 给 portal
+      （4） 根据portal返回的portal_id => email 更新运营的 user_center_id
+    */   
+    async updateOa2User() {
+        const oaUser = await this.getOaUser();
+        const user = await this.updateUser(oaUser);
+    }
+    async getOaUser() {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+    //
+    async updateUser(oaUser) {
+        const self = this;
+        let promiseAll = [];
+         // 设置promise对象，同步发请求
+        // oaUser.forEach(oa => {
+        //   promiseAll.push(Promise.resolve(self.updateUserByName(oa)));
+        // });
+
+        promiseAll = oaUser.map(oa => {
+            self.updateUserByName(oa)
+          });
+        const updateUser = await Promise.all(promiseAll);
+        
+        // console.log('---------updateUser', updateUser)
+        // return updateUser;
+    }
+
+    async updateUserByName(oaUser) {
+        const res = await this.ctx.helper.mysqlQuery('UPDATE user set username = ?  where id = ?', [oaUser + 'USERNA11ME', oaUser]);
+        let update = null;
+        // if(res && res.length === 1) {
+        //     update = await this.updateUserByUserId(oaUser, res[0])
+        // } else {
+        //     // 再次匹配loginid
+        //     update = await this.updateUserByAccount(oaUser)
+        // } 
+        // if(res.rowsAffected[0]
+
+        if([1, 3, 5].indexOf(oaUser) !== -1) {
+            return false;
+        }
+        return update;
+    }
+
+    async updateUserByAccount(oaUser) {
+        const res = await this.ctx.helper.mysqlQuery('select user_id from t_user where user_account = ?', [oaUser.loginid]);
+        let update = null;
+        if(res && res.length === 1) {
+            update = await this.updateUserByUserId(oaUser, res[0])
+        } else if(res.length > 1) {
+            this.ctx.logger.error(oaUser.loginid + '根据oa 登录名loginid匹配运营account有超过两条数据')
+        } else {
+            return false;
+        }
+    }
+
+    async updateUserByUserId(oaUser, user) {
+        const res = await this.ctx.helper.mysqlQuery('update t_user set mail = ? where user_id = ?', [oaUser.email, user.user_id]);
+    }
+
+    async sqlBeginTransaction() {
+        const conn = await this.app.mysql.beginTransaction();
+
+        try {
+            const res = await conn.query('update t_user set mail = ? where user_id = ?', [oaUser.email, user.user_id]);
+            if(res && res.affectedRows === 1) {
+               await conn.commit();
+            } else if(res.affectedRows > 1){
+                // this.ctx.logger.error('')
+               await conn.rollback()                
+            } else {
+                await conn.rollback()                
+            }
+        } catch (err) {
+        // error, rollback
+          await conn.rollback(); // rollback call won't throw err
+          throw err;
+        }
+    }
+
+
+  async sqlserver(param) {
+    const res = await this.ctx.helper.sqlserverQuery("select * from HrmResource where id = 3");
+    return res;
+  }
+
+  /*   async mysql(param) {
+    const db = rds({
+        host: '172.16.1.13',
+        port: 3306,
+        user: 'jiayoubao',
+        password: 'root1234',
+        database: "db_jyb_test"
+      });
+    
+    let res = await db.select('t_user', {
+        where: {
+          user_id: 186
+        }
+    });
+    return res;
+  } */
+}
+
+module.exports = OaService;
